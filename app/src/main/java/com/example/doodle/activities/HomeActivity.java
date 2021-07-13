6ee41e5091ef4802476e6ab504a41365a9aef2f8
Meghan Longtin Lin <mlin2024@fb.com>
2021-07-13 09:24:58 -0400

    Added toolbar to Home activity

--- app/src/main/java/com/example/doodle/activities/HomeActivity.java
+++ app/src/main/java/com/example/doodle/activities/HomeActivity.java
@@ -16,6 +16,7 @@ public class HomeActivity extends AppCompatActivity {
     public static final String TAG = "HomeActivity";
 
     RelativeLayout homeRelativeLayout;
+    private Toolbar toolbar;
 
 
     @Override
@@ -24,6 +25,38 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         homeRelativeLayout = findViewById(R.id.homeRelativeLayout);
+        toolbar = findViewById(R.id.homeActivityToolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.logoutMenuItem:
+                logout();
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 
     private void logout() {
